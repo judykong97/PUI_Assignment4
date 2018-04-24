c060e62769529bbdcd56184957a04d84632f989c
judykong97 <junhank@andrew.cmu.edu>
2018-04-24 03:32:28 -0400

    Final Project

---
+++ docs/assets/js/site.js
@@ -0,0 +1,5 @@
+// Rotate on load for the fun page
+var CSStransforms = anime({
+  targets: '.el',
+  rotate: '1turn'
+});
--- docs/assets/js/transform.js
+++ docs/assets/js/transform.js
@@ -1,28 +1,3 @@
-$(document).ready(function(){
-  // Initialize Tooltip
-  $('[data-toggle="tooltip"]').tooltip(); 
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#project-page']").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-      // Store hash
-      var hash = this.hash;
-      // Using jQuery's animate() method to add smooth page scroll
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      });
-    }
-  });
-})
-
-// Rotate on load for the fun page
-var CSStransforms = anime({
-  targets: '.el',
-  rotate: '1turn'
-});
-
 // Enlarge on hover, return back to initial size when mouse leaves
 var buttonEl1 = document.getElementById("CSStransforms1");
 function animateButton1(scale, duration, elasticity) {
